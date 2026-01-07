f0a0128f94181f1afc381962d4405214ad746e05
Naman <njain4282@gmail.com>
2026-01-07 12:08:41 +0530

    change api

--- app/api/subscription/start/route.js
+++ app/api/subscription/start/route.js
@@ -1,8 +1,7 @@
 import { NextResponse } from "next/server";
-import { PrismaClient } from "@prisma/client";
+import prisma from "@/lib/prisma";
 import { nanoid } from "nanoid"
 
-const prisma = new PrismaClient();
 
 export async function POST(req) {
     try {
