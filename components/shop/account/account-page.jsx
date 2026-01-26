cd81b917421dcb3ed5ac945c94c67a2826444d4b
Naman <njain4282@gmail.com>
2026-01-26 18:55:06 +0530

    error fix

--- components/shop/account/account-page.jsx
+++ components/shop/account/account-page.jsx
@@ -1,12 +1,8 @@
 
-import { useState } from "react"
 import ProfileCard from "./profile-card"
 import PasswordCard from "./password-card"
 
 export default function AccountPage() {
-
-  const [emailNotifications, setEmailNotifications] = useState(true)
-  const [securityAlerts, setSecurityAlerts] = useState(true)
 
   return (
     <div className="space-y-6">
